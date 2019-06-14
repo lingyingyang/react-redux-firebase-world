15375aebe377cd3cce1886c3399a434ac3699efb
lingyingyang <41569566+lingyingyang@users.noreply.github.com>
2019-06-14 16:45:09 +1000

    update navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -11,7 +11,7 @@ function Navbar(props) {
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
-                <Link to="/" className="brand-logo">MarioPlan</Link>
+                <Link to="/" className="brand-logo left">MarioPlan</Link>
                 {links}
             </div>
         </nav>
