681ca053fc0c9228ccd29dc1522a9cdd58fae751
lingyingyang <41569566+lingyingyang@users.noreply.github.com>
2019-06-16 13:14:21 +1000

    add pagination for projectList

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,21 +1,40 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary';
 import { Link } from 'react-router-dom'
+import Pagination from './Pagination';
 
-function ProjectList({ projects }) {
-    return (
-        <div className="project-list section">
-            {
-                projects && projects.map(project => {
+class ProjectList extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            projects: props.projects,
+            pageOfItems: []
+        }
+
+        // bind function in constructor instead of render (https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md)
+        this.onChangePage = this.onChangePage.bind(this);
+    }
+
+    onChangePage(pageOfItems) {
+        // update state with new page of items
+        this.setState({ pageOfItems: pageOfItems });
+    }
+
+    render() {
+        return (
+            <div className="project-list section">
+                {this.state.projects && this.state.pageOfItems.map(project => {
                     return (
                         <Link to={'/project/' + project.id} key={project.id}>
                             <ProjectSummary project={project} />
                         </Link>
                     )
-                })
-            }
-        </div>
-    )
+                })}
+                <Pagination items={this.state.projects} onChangePage={this.onChangePage} />
+            </div>
+        )
+    }
 }
 
 export default ProjectList
